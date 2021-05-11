56b47b84ba6b0e089b726c5990603cd20c40341a
flo <florianbracq42@gmail.com>
2021-05-11 16:41:08 +0200

    Implementation of packages for the functioning of the API

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const favicon = require('serve-favicon')
+const sequelize = require('./src/db/sequelize')
+const cors = require('cors')
+
+
+
+const app = express()
+const port = 3000
+
+
+
+app
+.use(cors())
+.use(morgan('dev'))
+.use(bodyParser.json())
+sequelize.initDb()
+
+// Here I place the endpoints
+
+
+
+// On ajoute la gestion des erreurs
+app.use(({res}) => {
+    const message = "Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL."
+    res.status(404).json({message})
+})
+
+app.listen(port, () => console.log(`notre application node est démarré sur : http://localhost:${port}`))
